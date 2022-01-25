3949e118b7bd5571ffba3a34500320b7c08fdd67
aiAdzari <gaoqaw@gmail.com>
2022-01-25 10:46:14 +0800

    Update 977. 有序数组的平方.js

--- JS/977. 有序数组的平方.js
+++ JS/977. 有序数组的平方.js
@@ -5,18 +5,19 @@
  var sortedSquares = function(nums) {
     var n = nums.length;
     var newarr = [];
-    for (let i = 0, j = n - 1, ind = n - 1; i <= n;) {
-        if (Math.sqrt(nums[i]) > Math.sqrt([nums[j])) {
-            newarr[ind] = Math.sqrt[nums[i]];
+    for (let i = 0, j = n - 1, ind = n - 1; i <= j;) {
+        if ((nums[i] * nums[i]) > nums[j] * nums[j]) {
+            newarr[ind] = nums[i] * nums[i];
             ++i;
         }
         else {
-            newarr[ind] = Math.sqrt[nums[j]];
+            newarr[ind] = nums[j] * nums[j];
             --j;
         }
         --ind;
     }
     return newarr;
 };
+
 
 console.log(sortedSquares([-4,-1,0,3,10]));
